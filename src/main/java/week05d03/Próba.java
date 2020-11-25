edb5e07a1362fbacc8e49056f0ee9c0393352297
gyuri <gyritter@digikabel.hu>
2020-11-25 15:48:35 +0100

    week05d03_2

--- src/main/java/week05d03/Próba.java
+++ src/main/java/week05d03/Próba.java
@@ -4,23 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Próba {
-    public int próba(List<String> words) {
+    public static int próba(List<String> words) {
 
         int counter = 0;
+
         for (String initial : words) {
-            if (initial.charAt(0) == 'A' || initial.charAt(0) == 'a') ;
-            counter += counter;
+            if ("A".equals(initial.charAt(0)) || "a".equals(initial.charAt(0))) counter += counter;
         }
         return counter;
     }
-    //public static void main(String[] args) {
-    //  private List<String> words=new ArrayList<>;
-    //  words.add("Alma");
-    //  words.add("tök");
-    //  words.add("andrea");
-    //  words.add("bivaly");
+    public static void main(String[] args) {
 
-    //  System.out.println(próba(words));
+      List<String> words = new ArrayList<>();
 
-    // }
+      words.add("Alma");
+      words.add("tök");
+      words.add("andrea");
+      words.add("bivaly");
+
+      System.out.println(próba(words));
+
+     }
 }
