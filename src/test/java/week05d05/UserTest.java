74de407ed85f83b055f250af961221461cbc487e
gyuri <gyritter@digikabel.hu>
2020-11-27 14:20:05 +0100

    javított teszt

--- src/test/java/week05d05/UserTest.java
+++ src/test/java/week05d05/UserTest.java
@@ -8,35 +8,23 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class UserTest {
 
     @Test
-    public void userTest(){
+    public void userTest() {
         User user = new User("John", "Doe", "abcd@akarmi.com");
         assertEquals("John Doe", user.getFullName());
     }
 
+
     @Test
-    public void userTest2(){
-        User user = new User("John", "Doe", "abcdakarmi.com");
-        assertEquals("John Doe", user.getFullName());
+    public void emailNoKukac() throws IllegalArgumentException {
+        Exception ex = assertThrows(IllegalArgumentException.class, () ->
+                new User("John", "Dow", "abcdakarmi.com"));
+        assertEquals("The e-mail address invalid!", ex.getMessage());
     }
 
     @Test
-    public void userTest3(){
-        User user = new User("John", "Doe", "abcd@akarmicom");
-        assertEquals("John Doe", user.getFullName());
+    public void emailNoPointAfterKukac() throws IllegalArgumentException {
+        Exception ex = assertThrows(IllegalArgumentException.class, () ->
+                new User("John", "Dow", "abcd@akarmicom"));
+        assertEquals("The e-mail address invalid!", ex.getMessage());
     }
-
-//    @Test
-//    public void emailNokukac() throws IllegalArgumentException {
-//        User user = new User("John", "Dow", "abcdakarmi.com");
-//        Exception ex = assertThrows(IllegalArgumentException.class, () -> user.kukac());
-//        assertEquals("The e-mail address invalid!", ex.getMessage());
-//    }
-
-//    @Test
-//    public void emailBadPointPosition() throws IllegalArgumentException {
-//        User user = new User("John", "Dow", "abc.d@akarmicom");
-//        Exception ex = assertThrows(IllegalArgumentException.class, () -> user);
-//        assertEquals("The e-mail address invalid!", ex.getMessage());
-//    }
-
 }
