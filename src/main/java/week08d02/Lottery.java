924c893436b00d93e21f2e53e0ca918b84e886a0
gyuri <gyritter@digikabel.hu>
2020-12-15 15:40:38 +0100

    2020.12.15.

---
+++ src/main/java/week08d02/Lottery.java
@@ -0,0 +1,40 @@
+package week08d02;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Lottery {
+
+   private List<Integer> lottery =new ArrayList<>();
+   Random rnd= new Random();
+
+    public List getNumbers(int interval, int numbers){
+        lottery.add(draw(interval));
+        int rndNumber;
+        boolean exists;
+        while (lottery.size()<numbers){
+            rndNumber=(draw(interval));
+            exists=false;
+            for(int rnd:lottery){
+                if (rndNumber == rnd){
+                   exists=true;
+                }
+            }
+            if (!(exists)) lottery.add(rndNumber);
+        }
+        return lottery;
+    }
+    public int draw(int interval) {
+        return rnd.nextInt(interval)+1;
+    }
+    public String toString(){
+        StringBuilder numbers=new StringBuilder();
+        for (int rnd:lottery){
+            numbers.append(rnd);
+            numbers.append(" ");
+
+        }
+        return numbers.toString().trim();
+    }
+}
