8739031bf1424895bbdea515bd210ecdcefbaa25
gyuri <gyritter@digikabel.hu>
2021-01-13 15:45:21 +0100

    2021.01.13.

---
+++ src/test/java/wek11d03/WordFilter.java
@@ -0,0 +1,25 @@
+package wek11d03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordFilter {
+
+    public List<String> wordsWithChar(List<String> words, char c){
+
+      List<String> result= new ArrayList();
+
+         String s= Character.toString(c)
+
+        for (String word:words){
+
+            if  (word.contains(s)){
+
+                result.add(word;
+            }
+
+        }
+        return result
+    }
+
+}
