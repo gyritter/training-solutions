a171cb71e63542c6bf2f82e269f86847ff496118
gyuri <gyritter@digikabel.hu>
2021-01-18 15:56:29 +0100

    2021.01.18.

---
+++ src/main/java/week12d01/GradeRounder.java
@@ -0,0 +1,16 @@
+package week12d01;
+
+public class GradeRounder {
+
+
+   public  int[] roundGrades(int[] grades){
+
+       for (int i=0; i<grades.length; i++){
+
+          if(grades[i]>40 &&((5*grades[i]/5+5)-grades[i]<3)){
+              grades[i]=5*((grades[i]/5)+1);
+          }
+       }
+       return grades;
+   }
+}
