8185e758ce28947a98a94d68b862229524239d94
gyuri <gyritter@digikabel.hu>
2021-02-15 20:28:29 +0100

    2021.02.15. exam03 nem találta a histogram.txt-t, mert másoláskor refaktorálta az elérését...

--- src/test/java/exam03/HistogramTest.java
+++ src/test/java/exam03/HistogramTest.java
@@ -12,9 +12,8 @@ class HistogramTest {
 
     @Test
     void createHistogram() throws IOException {
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(HistogramTest.class.getResourceAsStream("histogram/histogram.txt")))) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(HistogramTest.class.getResourceAsStream("histogram.txt")))) {
             String result = new Histogram().createHistogram(reader);
-//            System.out.println(result);
             assertEquals("***\n" +
                     "****\n" +
                     "**\n" +
